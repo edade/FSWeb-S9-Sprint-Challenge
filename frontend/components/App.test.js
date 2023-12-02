6f9bdc3bfd8663dfca1c11acdddf1a4f3082eede
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-02 03:56:55 +0300

    test yazıldı

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,20 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import AppFunctional from "./AppFunctional";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+test("uygulama render ediliyor", () => {
+  render(<AppFunctional />);
+});
+test("test initial state", () => {
+  render(<AppFunctional />);
+  const headings = screen.getAllByRole("heading", { level: 3 });
+  expect(headings).toHaveLength(3);
+  expect(headings[0]).toHaveTextContent("Koordinatlar (2, 2)");
+  expect(headings[1]).toHaveTextContent("0 kere ilerlediniz");
+  expect(headings[2]).toHaveTextContent("");
+});
+
